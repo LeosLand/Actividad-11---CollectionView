119b626f7a55c84aa4ae89978ca1131c71818c9a
LeosLand <30914387+LeosLand@users.noreply.github.com>
2018-04-17 21:30:09 -0500

    Programa Finalizado

---
+++ Activity_11-master/CollectionView_Intro/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  Created by Shadow on 20/03/18.
+//  Copyright © 2018 TecMilenio. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+
+}
+
